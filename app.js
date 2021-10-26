fa6a334b1555194d8a98d3d5e54f509599d808fe
Gasper Katrasnik <gkatrasnik@gmail.com>
2021-10-26 19:06:40 +0200

    mongo db variable set

--- app.js
+++ app.js
@@ -17,8 +17,9 @@ app.use(compression()); //Compress all routes
 
 //setting up mongoDB
 var mongoose = require("mongoose");
-var mongoDB =
+var dev_db_url =
   "mongodb+srv://gkatrasnik:canyon1995@cluster0.9jocm.mongodb.net/local_library?retryWrites=true&w=majority";
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
